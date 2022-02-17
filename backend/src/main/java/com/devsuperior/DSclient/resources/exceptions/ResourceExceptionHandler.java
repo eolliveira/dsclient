43f5a2b1905989c9027d4285c46a1cd75932cdd2
unknown <erick.oliveira9607@gmail.com>
2022-02-16 21:54:15 -0300

    Refactor ClientService

--- backend/src/main/java/com/devsuperior/DSclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/DSclient/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.DSclient.resources.exceptions;
 
+import com.devsuperior.DSclient.services.exceptions.DatabaseException;
 import com.devsuperior.DSclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,18 @@ public class ResourceExceptionHandler {
         StandartError standartError = new StandartError();
         standartError.setTimestamp(Instant.now());
         standartError.setError("Resource not found");
+        standartError.setMessage(e.getMessage());
+        standartError.setPath(request.getRequestURI());
+        standartError.setStatus(httpStatus);
+        return  ResponseEntity.status(httpStatus).body(standartError);
+    }
+
+    @ExceptionHandler(value = DatabaseException.class)
+    public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request){
+        int httpStatus = HttpStatus.BAD_REQUEST.value();
+        StandartError standartError = new StandartError();
+        standartError.setTimestamp(Instant.now());
+        standartError.setError("Database exception");
         standartError.setMessage(e.getMessage());
         standartError.setPath(request.getRequestURI());
         standartError.setStatus(httpStatus);
--- backend/src/main/java/com/devsuperior/DSclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/DSclient/services/ClientService.java
@@ -3,13 +3,16 @@ package com.devsuperior.DSclient.services;
 import com.devsuperior.DSclient.dto.ClientDTO;
 import com.devsuperior.DSclient.entities.Client;
 import com.devsuperior.DSclient.repositories.ClientRepository;
+import com.devsuperior.DSclient.services.exceptions.DatabaseException;
 import com.devsuperior.DSclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -18,14 +21,13 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable page) {
         Page<Client> clients = repository.findAll(page);
         return clients.map(ClientDTO::new);
     }
 
-
-    @Transactional
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Optional<Client> client = repository.findById(id);
         Client c = client.orElseThrow(() -> new ResourceNotFoundException("Resource id: " + id + " not found"));
@@ -43,20 +45,29 @@ public class ClientService {
 
     @Transactional
     public ClientDTO update(ClientDTO dto, Long id) {
-        Optional<Client> c = repository.findById(id);
-        Client client = c.orElseThrow(() -> new ResourceNotFoundException("Resource id: " + id + " not found"));
-        dtoForEntity(dto, client);
-        client = repository.save(client);
-        return new ClientDTO(client);
-
+        try{
+            Client client = repository.getOne(id);
+            dtoForEntity(dto, client);
+            client = repository.save(client);
+            return new ClientDTO(client);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Client id:" + id + " not found");
+        }
     }
 
     @Transactional
     public void delete(Long id) {
-        //Client client = repository.getOne(id);
-        Optional<Client> client = repository.findById(id);
-        Client c = client.orElseThrow(() -> new ResourceNotFoundException("Resource id: " + id + " not found"));
-        repository.deleteById(c.getId());
+        try {
+            Client client = repository.getOne(id);
+            repository.deleteById(client.getId());
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Client id:" + id + " not found");
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new DatabaseException("Database integrity violation");
+        }
     }
 
     private void dtoForEntity(ClientDTO dto, Client client) {
